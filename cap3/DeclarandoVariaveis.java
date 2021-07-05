e7144bee83a13b67ddcee5c748f488fadbf3dc35
herbert henrique <bert@pop-os.localdomain>
2021-07-04 22:30:18 -0300

    aprendi a declarar e iniciar variaveis e os operadores aritimeticos

---
+++ cap3/DeclarandoVariaveis.java
@@ -0,0 +1,9 @@
+class DeclarandoVariaveis {
+  public static void main(String[] args) {
+    int idade = 20;
+    System.out.println(idade);
+    
+    int idadeAnoQueVem = idade + 1;
+    System.out.println(idadeAnoQueVem);
+  }
+}
---
+++ cap3/OperadoresAritmeticos.java
@@ -0,0 +1,15 @@
+class OperadoresAritmeticos {
+  public static void main(String[] args) {
+    int quatro = 2 + 2; //adição
+    int tres = 5 - 2; //subtração
+    int oito = 4 * 2; //multiplicação
+    int dezesseis = 64 / 4;
+    int um = 5 % 2; //resto da divisão inteira 
+    
+    System.out.println(quatro);
+    System.out.println(tres);
+    System.out.println(oito);
+    System.out.println(dezesseis);
+    System.out.println(um);
+  }
+}
